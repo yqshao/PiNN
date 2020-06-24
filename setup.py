06c6ec44caa875151a738e5af3eb93a33a52ed13
Yunqi Shao <yunqi@kemi.uu.se>
2020-06-24 09:55:11 +0200

    [core] update TensorFlow requirement

--- setup.py
+++ setup.py
@@ -3,14 +3,18 @@ from setuptools import setup, find_packages
 
 setup(name='pinn',
       version='dev',
-      description='Pair interaction neural network',
+      description='Pair interaction Neural Network',
       url='https://github.com/yqshao/pinn',
       author='Yunqi Shao',
       author_email='yunqi_shao@yahoo.com',
       license='BSD',
       packages=find_packages(),
-      install_requires=['ase', 'numpy', 'pyyaml', 'tensorflow>=2.0'],
-      entry_points={
-          'console_scripts': ['pinn_train=pinn.trainer:main']
-      }
+      install_requires=['numpy>1.3.0',
+                        'ase>=3.19.0',
+                        'pyyaml>=3.01'],
+      python_requires='>=3.6',
+      extras_require={'cpu': ['tensorflow-cpu>=2.1'],
+                      'gpu': ['tensorflow>=2.1']},
+      entry_points={'console_scripts':
+                    ['pinn_train=pinn.trainer:main']}
 )
